c659e25dd21e9b7bd7cf34fd7f99940fab49eb14
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-03-29 12:00:41 +0200

    clock hecho

--- clock.ts
+++ clock.ts
@@ -1,21 +1,96 @@
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  hour: number;
+  minute: number;
+  constructor(hour: number, minute?: number) {
+    if (minute == undefined) {
+      minute = 0;
+    } else {
+
+      if (minute < 0) {
+        let minus: number = Math.floor(minute / 60);
+        minute = 60 + minute% 60;
+        hour = hour + minus;
+        if (hour<0) {
+          while(hour<0){
+            hour=24+hour;
+          }
+        }
+      }
+      if (hour < 0) {
+        let substract: number = hour % 24;
+        hour = 24 + substract;
+      }
+
+
+      if (minute >= 60) {
+        let add: number = Math.floor(minute / 60);
+        minute = minute % 60;
+        hour = hour + add;
+      }
+      if (hour >= 24) {
+        hour = hour % 24;
+      }
+
+    }
+    this.hour = hour;
+    this.minute = minute;
+
   }
 
-  public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public toString(): string {
+    function pad2(number: number) {
+      return (number < 10 ? '0' : '') + number
+
+    }
+    return (pad2(this.hour) + ":" + pad2(this.minute))
   }
 
-  public plus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public plus(minutes: number): Clock {
+    let hour: number
+    hour = this.hour;
+    minutes = this.minute + minutes;
+
+    if (minutes >= 60) {
+      let add: number = Math.floor(minutes / 60);
+      minutes = minutes % 60;
+      hour = hour + add;
+      if (hour >= 24) {
+        hour = hour % 24;
+      }
+    }
+
+    this.minute = minutes;
+    this.hour = hour;
+    return this;
   }
 
-  public minus(minutes: unknown): Clock {
-    throw new Error('Remove this statement and implement this function')
+  public minus(minutes: number): Clock {
+    let hour: number
+    hour = this.hour;
+    
+    minutes = this.minute - minutes;
+    if (minutes < 0) {
+      let minus: number = Math.floor(minutes / 60);
+      minutes = 60 + minutes % 60;
+      hour = hour + minus;
+      if (hour<0) {
+        while(hour<0){
+          hour=24+hour;
+        }
+      }
+    }
+
+    this.minute = minutes;
+    this.hour = hour;
+    return this;
   }
 
-  public equals(other: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public equals(other: Clock): boolean {
+    if(JSON.stringify(this) === JSON.stringify(other)){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
+
