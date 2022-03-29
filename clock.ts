0baff9c42b00034be36dcb6b7ba0f40f6eb2678e
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-03-29 13:55:12 +0200

    55 linias

--- clock.ts
+++ clock.ts
@@ -1,94 +1,54 @@
 export class Clock {
   hour: number;
   minute: number;
-  constructor(hour: number, minute?: number) {
-    if (minute == undefined) {
-      minute = 0;
-    } else {
-
-      if (minute < 0) {
-        let minus: number = Math.floor(minute / 60);
-        minute = 60 + minute% 60;
-        hour = hour + minus;
-        if (hour<0) {
-          while(hour<0){
-            hour=24+hour;
-          }
-        }
+  constructor(hour: number, minutes?: number) {
+    this.hour=0;
+    this.minute=0;
+    if(minutes==undefined){
+      this.hour=hour;
+      minutes==0
+    }else{
+      let total:number=minutes+hour*60;
+      if(total>0){
+        this.plus(total);
+      }else{
+        console.log(-total);
+        this.minus(-total);
       }
-      if (hour < 0) {
-        let substract: number = hour % 24;
-        hour = 24 + substract;
-      }
-
-
-      if (minute >= 60) {
-        let add: number = Math.floor(minute / 60);
-        minute = minute % 60;
-        hour = hour + add;
-      }
-      if (hour >= 24) {
-        hour = hour % 24;
-      }
-
-    }
-    this.hour = hour;
-    this.minute = minute;
-
+    } 
   }
-
   public toString(): string {
     function pad2(number: number) {
       return (number < 10 ? '0' : '') + number
-
     }
     return (pad2(this.hour) + ":" + pad2(this.minute))
   }
-
   public plus(minutes: number): Clock {
-    let hour: number
-    hour = this.hour;
-    minutes = this.minute + minutes;
-
-    if (minutes >= 60) {
-      let add: number = Math.floor(minutes / 60);
-      minutes = minutes % 60;
-      hour = hour + add;
-      if (hour >= 24) {
-        hour = hour % 24;
-      }
-    }
-
-    this.minute = minutes;
-    this.hour = hour;
+    let total:number= this.hour*60+this.minute;
+    total= total + minutes;
+    this.hour=Math.trunc(total / 60)%24;
+    this.minute=total%60;
     return this;
   }
-
   public minus(minutes: number): Clock {
-    let hour: number
-    hour = this.hour;
-    
-    minutes = this.minute - minutes;
-    if (minutes < 0) {
-      let minus: number = Math.floor(minutes / 60);
-      minutes = 60 + minutes % 60;
-      hour = hour + minus;
-      if (hour<0) {
-        while(hour<0){
-          hour=24+hour;
-        }
+    let total:number= this.hour*60+this.minute;
+    total= total - minutes;
+      this.hour=Math.trunc(total / 60)%24;
+      this.minute=total%60;
+    if(total<0){
+      this.hour=this.hour+24-1;
+      this.minute=this.minute+60;
+      if(this.minute==60){
+        this.minute=0
+        this.hour=this.hour+1;
       }
     }
-
-    this.minute = minutes;
-    this.hour = hour;
     return this;
   }
-
   public equals(other: Clock): boolean {
-    if(JSON.stringify(this) === JSON.stringify(other)){
+    if (JSON.stringify(this) === JSON.stringify(other)) {
       return true;
-    }else{
+    } else {
       return false;
     }
   }
