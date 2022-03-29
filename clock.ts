21d952c0693047778064a020ead6fd482b2ebfe6
EricGoyenecheaEsquina <102578352+EricGoyenecheaEsquina@users.noreply.github.com>
2022-03-29 16:05:05 +0200

    32 lineas

--- clock.ts
+++ clock.ts
@@ -1,49 +1,26 @@
 export class Clock {
-  hour: number;
   minute: number;
-  constructor(hour: number, minutes?: number) {
-    this.hour=0;
-    this.minute=0;
-    if(minutes==undefined){
-      this.hour=hour;
-      minutes==0
-    }else{
-      let total:number=minutes+hour*60;
-      if(total>0){
-        this.plus(total);
-      }else{
-        console.log(-total);
-        this.minus(-total);
-      }
-    } 
+  hour: number;
+  constructor(hour: number=0, minutes: number = 0,total:number=0) {
+    (total!=0) ? total=total : total=hour*60+minutes;
+    (total > 0) ? this.hour=Math.trunc(total/60)%24: this.hour=Math.trunc(total/60)%24+24-1;
+    (total > 0) ? this.minute=total%60: this.minute=total%60+60;
+    if(this.minute==60){
+      this.minute=0
+      this.hour=this.hour+1;
+    }
   }
   public toString(): string {
     function pad2(number: number) {
-      return (number < 10 ? '0' : '') + number
+      return (number < 10 ? '0' : '') + number;
     }
     return (pad2(this.hour) + ":" + pad2(this.minute))
   }
   public plus(minutes: number): Clock {
-    let total:number= this.hour*60+this.minute;
-    total= total + minutes;
-    this.hour=Math.trunc(total / 60)%24;
-    this.minute=total%60;
-    return this;
+    return(new Clock(this.hour,this.minute,(this.hour*60+this.minute+minutes)));
   }
   public minus(minutes: number): Clock {
-    let total:number= this.hour*60+this.minute;
-    total= total - minutes;
-      this.hour=Math.trunc(total / 60)%24;
-      this.minute=total%60;
-    if(total<0){
-      this.hour=this.hour+24-1;
-      this.minute=this.minute+60;
-      if(this.minute==60){
-        this.minute=0
-        this.hour=this.hour+1;
-      }
-    }
-    return this;
+    return(new Clock(this.hour,this.minute,(this.hour*60+this.minute-minutes)));
   }
   public equals(other: Clock): boolean {
     if (JSON.stringify(this) === JSON.stringify(other)) {
